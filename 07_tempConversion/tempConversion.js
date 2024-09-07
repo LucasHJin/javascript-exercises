128d9051aa86a03863ab842b16b60879401c1bd5
Lucas Jin <kblazer20@gmail.com>
2024-09-07 12:33:18 -0400

    Finished arrays and loops lesson

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let cel = Math.round(((F-32)*5/9)*10)/10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let far = Math.round((C*9/5+32)*10)/10;
+  return far;
 };
 
 // Do not edit below this line
