2bc69224cf02e41bc6fc6f44b7f8e27cfd6f9f76
Lucas Jin <kblazer20@gmail.com>
2024-09-11 19:26:35 -0400

    Finished additional js practice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num<0) {
+        return "OOPS";
+    }
+    if (num==0) {
+        return 0;
+    }
+    if (num==1 || num==2) {
+        return 1;
+    }
+    let prev = 1;
+    let prev2 = 1;
+    let ans = 2;
+    for (let i=3; i<num; i++) {
+        prev2 = ans;
+        ans += prev;
+        prev = prev2;
+    }
+    return ans;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function findAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    const ans = people.reduce((oldest, curr) => {
+        let oldAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currAge = findAge(curr.yearOfBirth, curr.yearOfDeath);
+
+        if (currAge>oldAge) {
+            return curr;
+        } else {
+            return oldest;
+        }
+    });
+    return ans;
 };
 
 // Do not edit below this line
